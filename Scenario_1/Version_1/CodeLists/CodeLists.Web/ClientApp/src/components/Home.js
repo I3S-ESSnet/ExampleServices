654b07362256f267053a1f791d5ae6a1a8e964dc
patrik.ahlen <patrik.ahlen@scb.se>
2020-01-16 12:26:20 +0100

    adding CodeLists project from HelloWorld template of .net core reactjs template

---
+++ Scenario_1/Version_1/CodeLists/CodeLists.Web/ClientApp/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Code Lists</h1>
+        <p>This application is where you manage your metadata.</p>
+
+      </div>
+    );
+  }
+}
