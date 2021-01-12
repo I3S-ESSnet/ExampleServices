0aa4c143dc4b677b4c1e9fc923b32311bad96ded
patrik.ahlen <patrik.ahlen@scb.se>
2021-01-12 14:10:02 +0100

    showing questions returned from api for weather survey, without form and design

--- Scenario_1/1_Starting/CAWI/CAWI/ClientApp/src/components/Survey.js
+++ Scenario_1/1_Starting/CAWI/CAWI/ClientApp/src/components/Survey.js
@@ -14,26 +14,38 @@ export class Survey extends Component {
 
     static renderQuestionTable(questions) {
         return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {questions.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
+            <div>
+                {/* <form></form> */}
+                {questions.map(question =>
+                    <td>
+                        <h1>{question.caption}</h1>
+                        <select>
+                            {question.options.map(option =>
+                            <option value="{option.value}">{option.caption}</option>    
+                            )}                            
+                        </select>
+                    </td>)}
+            </div>
+            // <table className='table table-striped' aria-labelledby="tabelLabel">
+            //     <thead>
+            //         <tr>
+            //             <th>Date</th>
+            //             <th>Temp. (C)</th>
+            //             <th>Temp. (F)</th>
+            //             <th>Summary</th>
+            //         </tr>
+            //     </thead>
+            //     <tbody>
+            //         {questions.map(forecast =>
+            //             <tr key={forecast.date}>
+            //                 <td>{forecast.date}</td>
+            //                 <td>{forecast.temperatureC}</td>
+            //                 <td>{forecast.temperatureF}</td>
+            //                 <td>{forecast.summary}</td>
+            //             </tr>
+            //         )}
+            //     </tbody>
+            // </table>
         );
     }
 
@@ -52,7 +64,7 @@ export class Survey extends Component {
     }
 
     async populateQuestions() {
-        const response = await fetch('weatherforecast');
+        const response = await fetch('survey');
         const data = await response.json();
         this.setState({ questions: data, loading: false });
     }
