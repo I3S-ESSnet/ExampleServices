e3eb477c8fa6eb32aa00cfdda659551b208b024e
patrik.ahlen <patrik.ahlen@scb.se>
2021-01-15 08:02:04 +0100

    connected service to errorlocalization service on port 8081, adjusted gui with new datacontract

--- Scenario_1/1_Starting/CAWI/CAWI/Controllers/SurveyController.cs
+++ Scenario_1/1_Starting/CAWI/CAWI/Controllers/SurveyController.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using CAWI.Ui.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -13,12 +17,14 @@ namespace CAWI.Ui.Controllers
     public class SurveyController : ControllerBase
     {
         private readonly ILogger<SurveyController> _logger;
-
+        private const string Weather = "weather";
+        private const string Country = "country";
+        
         public SurveyController(ILogger<SurveyController> logger)
         {
             _logger = logger;
         }
-        
+
         [HttpGet]
         public IEnumerable<Question> GetQuestions()
         {
@@ -27,23 +33,56 @@ namespace CAWI.Ui.Controllers
                 new Question
                 {
                     Caption = "Where are you?",
-                    Variable = "Country",
+                    Variable = Country,
                     Options = ReadCountries().Select(x=> new Option {Caption = x.Name, Value = x.Code})
                 },
                 new Question
                 {
                     Caption = "How is your weather?",
-                    Variable = "Weather",
+                    Variable = Weather,
                     Options = CreateWeatherOptions()
                 }
             };
         }
-        
+
         [HttpPost]
-        public IEnumerable<AnswerValidation> PostAnswers(IEnumerable<Answer> answers)
+        public async Task<IEnumerable<Error>> PostAnswers(IEnumerable<Answer> answers)
         {
-            return answers.ToList().Select(x => new AnswerValidation()
-                {Variable = x.Variable, Value = x.Value, Result = "Plausable"});
+            var validationResult = await ValidateAnswers(answers);
+            
+            return validationResult;
+        }
+
+        public async Task<IEnumerable<Error>> ValidateAnswers(IEnumerable<Answer> answers)
+        {
+            var country = answers.FirstOrDefault(x => x.Variable.ToLower() == Country);
+            var weather = answers.FirstOrDefault(x => x.Variable.ToLower() == Weather);
+            
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var uri = "http://localhost:8081" + $"/api/validate?country={country?.Variable}&weather={weather?.Value}";
+                    var content = new MultipartFormDataContent
+                    {
+                        {new StringContent("country"), country?.Variable},
+                        {new StringContent("weather"), weather?.Variable}
+                    };
+                    using (var response = await client.PostAsync(uri, content))
+                    {
+                        var apiResponse = await response.Content.ReadAsStringAsync();
+                        var errors = JsonConvert.DeserializeObject<List<Error>>(apiResponse);
+                        return errors;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    _logger.LogError(e, "Fault validating answers", answers);
+                    throw;
+                }
+
+            }
         }
 
         private IEnumerable<Option> CreateWeatherOptions()
@@ -52,7 +91,7 @@ namespace CAWI.Ui.Controllers
             {
                 new Option {Caption = "Snow", Value = "Snow"},
                 new Option {Caption = "Rain", Value = "Rain"},
-                new Option {Caption = "Sun", Value = "Sun"}, 
+                new Option {Caption = "Sun", Value = "Sun"},
                 new Option {Caption = "Fog", Value = "Fog"},
                 new Option {Caption = "Hail", Value = "Hail"},
                 new Option {Caption = "Cloud", Value = "Cloud"}
