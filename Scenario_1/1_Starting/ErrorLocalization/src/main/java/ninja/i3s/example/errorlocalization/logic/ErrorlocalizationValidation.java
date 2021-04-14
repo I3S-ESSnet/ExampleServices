e722b7d8da845b9d602a327c0d123bcbf099537d
patrik.ahlen <patrik.ahlen@scb.se>
2021-04-14 10:35:38 +0200

    added logic for validating weather depending on choosen country

--- Scenario_1/1_Starting/ErrorLocalization/src/main/java/ninja/i3s/example/errorlocalization/logic/ErrorlocalizationValidation.java
+++ Scenario_1/1_Starting/ErrorLocalization/src/main/java/ninja/i3s/example/errorlocalization/logic/ErrorlocalizationValidation.java
@@ -1,14 +1,5 @@
 package ninja.i3s.example.errorlocalization.logic;
 
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
-import java.net.http.HttpResponse;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.URI;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.time.Duration;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,47 +15,24 @@ public class ErrorlocalizationValidation {
         //Pack response
         ArrayList<ErrorlocalizationValidationError> errors = new ArrayList<ErrorlocalizationValidationError>();
 
-        // final HttpClient client = HttpClient.newBuilder()
-        //         .version(HttpClient.Version.HTTP_1_1)
-        //         .connectTimeout(Duration.ofSeconds(10))
-        //         .build();
-
-        // HttpRequest request1 = HttpRequest.newBuilder()
-        //         .uri(URI.create("http://localhost:5010"))
-        //         .build();
-
-        // try {
-        //     HttpResponse<String> response1;
-        //     response1 = (HttpResponse<String>) client.newBuilder()
-        //             .build()
-        //             .send(request1, BodyHandlers.ofString());
-        //     System.out.println(response1.body());
-        // } catch (Exception ex) {
-        //     error = new ErrorlocalizationValidationError("Codelist service not available", "000", ex.getLocalizedMessage());
-        //     errors.add(error);
-        // }
-
         // Read countries from file
-        
         List<Country> countries = CountryParser.parse();
 
-        
-        countries.isEmpty();
-
         List<String> countryCodes = countries.stream().map(x -> x.getCode()).collect((Collectors.toList()));
-        
 
         //Rule 1 : check if the country is in the allowed list              
         if (!countryCodes.contains(country)) {
             error = new ErrorlocalizationValidationError("country not allowed", "001", "Country not in the list");
-            
             errors.add(error);
-
         }
 
         //Rule 2 : check if the weather allowed for the counrty
-        error = new ErrorlocalizationValidationError("weather not allowed", "002", "weather not allowed");
-        errors.add(error);
+        var countryWeather = new CountryWeather(weather, country);
+
+        if (!countryWeather.isValid()) {
+            error = new ErrorlocalizationValidationError("weather not allowed", "002", "weather not allowed");
+            errors.add(error);
+        }        
 
         return errors;
     }
